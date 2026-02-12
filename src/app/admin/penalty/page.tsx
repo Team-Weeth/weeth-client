4a659d98aa6a6726a37d116b3b6e98bd0fcd7b2f
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-02-12 14:34:42 +0900

    chore: 넥스트 초기세팅

---
+++ src/app/admin/penalty/page.tsx
@@ -0,0 +1,3 @@
+export default function PenaltyPage() {
+  return <div>PenaltyPage</div>;
+}
---
+++ src/lib/actions/index.ts
@@ -0,0 +1,2 @@
+// Next.js Server Actions 파일을 모아두는 곳입니다.
+// "use server" 지시어를 사용해서 서버에서 직접 실행되는 함수들입니다.
