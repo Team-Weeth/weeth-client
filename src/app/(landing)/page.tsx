bc802d38166b629cc510700346fe49afcdf81fe6
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-02-12 15:39:10 +0900

    feat: 디폴트 폰트 확인용

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,3 +1,17 @@
 export default function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <div className="flex h-screen flex-col gap-1">
+      <div className="typo-h1">LandingPage</div>
+      <div className="typo-h2">LandingPage</div>
+      <div className="typo-h3">LandingPage</div>
+      <div className="typo-sub1">LandingPage</div>
+      <div className="typo-sub2">LandingPage</div>
+      <div className="typo-body1">LandingPage</div>
+      <div className="typo-body2">LandingPage</div>
+      <div className="typo-caption1">LandingPage</div>
+      <div className="typo-caption2">LandingPage</div>
+      <div className="typo-button1">LandingPage</div>
+      <div className="typo-button2">LandingPage</div>
+    </div>
+  );
 }
