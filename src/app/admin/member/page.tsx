c7eb487d58bfd88f5ab0bcc425bb8d62a4306beb
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-02-12 15:39:01 +0900

    feat: 어드민 폰트 확인용

--- src/app/admin/member/page.tsx
+++ src/app/admin/member/page.tsx
@@ -1,3 +1,17 @@
 export default function MemberPage() {
-  return <div>MemberPage</div>;
+  return (
+    <div className="flex h-screen flex-col gap-1">
+      <div className="typo-h1">MemberPage</div>
+      <div className="typo-h2">MemberPage</div>
+      <div className="typo-h3">MemberPage</div>
+      <div className="typo-sub1">MemberPage</div>
+      <div className="typo-sub2">MemberPage</div>
+      <div className="typo-body1">MemberPage</div>
+      <div className="typo-body2">MemberPage</div>
+      <div className="typo-caption1">MemberPage</div>
+      <div className="typo-caption2">MemberPage</div>
+      <div className="typo-button1">MemberPage</div>
+      <div className="typo-button2">MemberPage</div>
+    </div>
+  );
 }
