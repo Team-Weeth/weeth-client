7e60ed4ec7b8d4ee5ac1c6568d1f4da3ae623d93
Dahyeon <dahhyeon22@gmail.com>
2026-03-02 17:14:35 +0900

    feat: shadcn의 card 사용해서  list item component 구현

--- src/app/(public)/attendance/page.tsx
+++ src/app/(public)/attendance/page.tsx
@@ -1,9 +1,22 @@
+'use client';
+
 import { AttendanceProgressBar } from '@/components/attendance/AttendanceProgressBar';
+import { Card } from '@/components/ui/card';
 
 export default function AttendancePage() {
   return (
-    <div>
+    <div className="flex flex-col gap-6 px-6">
       <AttendanceProgressBar attendanceRate={80} />
+
+      <Card
+        variant="buttonSet"
+        overline="오늘의 출석"
+        title="프론트엔드 중간 발표"
+        description="날짜: 2024년 7월 18일 (7:00 PM~8:30 PM)\n장소: 가천관 247호"
+        onPrimaryClick={() => console.log('출석하기')}
+        onSecondaryClick={() => console.log('출석코드 확인')}
+        showArrow
+      />
     </div>
   );
 }
