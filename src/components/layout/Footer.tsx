2f46ba8808c62e484b5a05b8b7aeb45a263b643b
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-03-03 17:31:57 +0900

    feat: footer 컴포넌트 추가

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+
+interface FooterProps {
+  isSmall?: boolean;
+}
+
+export default function Footer({ isSmall = false }: FooterProps) {
+  return (
+    <>
+      {!isSmall && (
+        <footer className="bg-container-neutral-alternative flex flex-col items-center justify-center gap-[67px] px-[18px] py-[26px]">
+          <div className="flex w-[992px] gap-4">
+            <div className="flex flex-col gap-200">
+              <p className="typo-caption1 text-text-normal">Leets Makers</p>
+              <p className="typo-body2 text-text-alternative">Weeth Admin</p>
+              <p className="typo-body2 text-text-alternative">Leets Makers Site</p>
+              <p className="typo-body2 text-text-alternative">문의 메일</p>
+            </div>
+            <div className="flex flex-col gap-200">
+              <p className="typo-caption1 text-text-normal">Leets</p>
+              <p className="typo-body2 text-text-alternative">Leets Site</p>
+              <p className="typo-body2 text-text-alternative">문의 메일</p>
+            </div>
+          </div>
+          <div className="flex w-[992px] flex-col gap-[10px]">
+            <Image src="/assets/logo/logo_full_Footer.svg" width={90} height={40} alt="logo" />
+            <p className="typo-caption1 text-text-disabled">© Weeth ALL RIGHTS RESERVED.</p>
+          </div>
+        </footer>
+      )}
+      <footer className="bg-container-neutral-alternative flex w-[304px] flex-col gap-[67px] rounded-lg px-[18px] py-[26px]">
+        <div className="flex flex-col gap-200">
+          <p className="typo-caption1 text-text-normal">Weeth 서비스</p>
+          <p className="typo-body2 text-text-alternative">서비스 소개</p>
+          <p className="typo-body2 text-text-alternative">관리자 서비스</p>
+          <p className="typo-body2 text-text-alternative">문의 메일</p>
+        </div>
+        <div className="flex flex-col gap-[10px]">
+          <Image src="/assets/logo/logo_full_Footer.svg" width={90} height={40} alt="logo" />
+          <p className="typo-caption1 text-text-disabled">© Weeth ALL RIGHTS RESERVED.</p>
+        </div>
+      </footer>
+    </>
+  );
+}
