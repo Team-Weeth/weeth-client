d42dc1c759a9f32c80037dd93c24f592e8ec8ddc
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-03-03 17:36:37 +0900

    fix: footer isSmall 분기처리

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,7 +7,7 @@ interface FooterProps {
 export default function Footer({ isSmall = false }: FooterProps) {
   return (
     <>
-      {!isSmall && (
+      {!isSmall ? (
         <footer className="bg-container-neutral-alternative flex flex-col items-center justify-center gap-[67px] px-[18px] py-[26px]">
           <div className="flex w-[992px] gap-4">
             <div className="flex flex-col gap-200">
@@ -27,19 +27,20 @@ export default function Footer({ isSmall = false }: FooterProps) {
             <p className="typo-caption1 text-text-disabled">© Weeth ALL RIGHTS RESERVED.</p>
           </div>
         </footer>
+      ) : (
+        <footer className="bg-container-neutral-alternative flex w-[304px] flex-col gap-[67px] rounded-lg px-[18px] py-[26px]">
+          <div className="flex flex-col gap-200">
+            <p className="typo-caption1 text-text-normal">Weeth 서비스</p>
+            <p className="typo-body2 text-text-alternative">서비스 소개</p>
+            <p className="typo-body2 text-text-alternative">관리자 서비스</p>
+            <p className="typo-body2 text-text-alternative">문의 메일</p>
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <Image src="/assets/logo/logo_full_Footer.svg" width={90} height={40} alt="logo" />
+            <p className="typo-caption1 text-text-disabled">© Weeth ALL RIGHTS RESERVED.</p>
+          </div>
+        </footer>
       )}
-      <footer className="bg-container-neutral-alternative flex w-[304px] flex-col gap-[67px] rounded-lg px-[18px] py-[26px]">
-        <div className="flex flex-col gap-200">
-          <p className="typo-caption1 text-text-normal">Weeth 서비스</p>
-          <p className="typo-body2 text-text-alternative">서비스 소개</p>
-          <p className="typo-body2 text-text-alternative">관리자 서비스</p>
-          <p className="typo-body2 text-text-alternative">문의 메일</p>
-        </div>
-        <div className="flex flex-col gap-[10px]">
-          <Image src="/assets/logo/logo_full_Footer.svg" width={90} height={40} alt="logo" />
-          <p className="typo-caption1 text-text-disabled">© Weeth ALL RIGHTS RESERVED.</p>
-        </div>
-      </footer>
     </>
   );
 }
