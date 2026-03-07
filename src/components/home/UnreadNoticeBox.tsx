9b5715c5ad8d78b072ce613a1ca35300ff2b3b0d
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-03-07 21:41:35 +0900

    fix: alt명 수정

--- src/components/home/UnreadNoticeBox.tsx
+++ src/components/home/UnreadNoticeBox.tsx
@@ -11,7 +11,7 @@ export function UnreadNoticeBox() {
       <div className="bg-container-neutral flex flex-col gap-[5px] rounded-b-lg px-450 py-400">
         <div className="flex gap-[5px]">
           <p className="typo-sub2 text-text-strong">이번주는 중간고사로 쉬어갑니다</p>
-          <Image src={NewIcon} alt="delete" width={9} height={16} />
+          <Image src={NewIcon} alt="new" width={9} height={16} />
         </div>
         <p className="typo-button2 text-text-normal w-[604px]">
           여러분 벌써 중간고사가 다가왔습니다. 다들 열심히 시험공부 잘 하고 계신가요? 릿츠 4기와
