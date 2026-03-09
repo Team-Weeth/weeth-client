76f7912495e00836709acf21a3f2b9d7748fda96
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-03-09 16:37:13 +0900

    fix: 동아리 정보 컨텐츠 정렬 수정

--- src/components/home/ClubInfo.tsx
+++ src/components/home/ClubInfo.tsx
@@ -1,27 +1,29 @@
-import { Avatar, AvatarImage, AvatarFallback, Button } from '@/components/ui';
+import { Avatar, AvatarImage, AvatarFallback, Button, Divider } from '@/components/ui';
 import { PeopleIcon } from '@/assets/icons';
 import Image from 'next/image';
 
 export function ClubInfo() {
   return (
     <>
-      <div className="mt-[18px] mb-5 flex items-center justify-center gap-4 px-200">
-        <Avatar>
-          <AvatarImage width={56} height={56} src="https://github.com/shadcn.png" />
+      <div className="flex items-center gap-4 px-200 py-300">
+        <Avatar type="square">
+          <AvatarImage width={64} height={64} src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
-        <div className="flex flex-col gap-[2px]">
+        <div className="flex w-[172px] flex-col justify-center gap-[2px]">
           <p className="text-text-strong typo-sub1">가천대 검도부</p>
           <p className="text-text-normal typo-body2">날씨가 춥네요, 건강이 최고</p>
         </div>
       </div>
-      {/* Divider 추가 */}
-      <div className="flex justify-between px-[10px] py-[18px]">
-        <div className="flex gap-[10px]">
+      <Divider />
+      <div className="flex justify-between px-[10px] py-450">
+        <div className="flex gap-200">
           <Image src={PeopleIcon} alt="people" width={20} height={20} />
           <p className="typo-button2 text-text-normal">368명</p>
         </div>
-        <a className="typo-button2 text-text-alternative underline underline-offset-2">초대</a>
+        <a className="typo-button2 text-text-alternative cursor-pointer underline underline-offset-2">
+          초대
+        </a>
       </div>
       <Button variant="secondary" size="md" className="w-full">
         글쓰기
