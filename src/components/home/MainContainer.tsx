c40c2ee30e2e2d67f0c8e4c6b0e4ca17c2b4d0b8
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-03-25 21:20:00 +0900

    feat: 메인 컨테이너 컴포넌트에 board 컴포넌트 추가

--- src/components/home/MainContainer.tsx
+++ src/components/home/MainContainer.tsx
@@ -1,9 +1,11 @@
 import { UnreadNoticeBox } from '@/components/home';
+import { BoardContent } from '../board';
 
 export function MainContainer() {
   return (
     <>
       <UnreadNoticeBox />
+      <BoardContent />
     </>
   );
 }
