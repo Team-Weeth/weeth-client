e530b087846def77bc58aaa8509fd5716bb0f3c3
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-03-29 16:22:07 +0900

    feat: 동아리 프로필 헤더 컴포넌트 구현 및 api 연동

---
+++ src/components/home/ClubProfileHeader.tsx
@@ -0,0 +1,26 @@
+import { BasicAvatarIcon } from '@/assets/icons';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui';
+import { useHomeQuery } from '@/hooks/home';
+import Image from 'next/image';
+
+export function ClubProfileHeader() {
+  const { data } = useHomeQuery();
+
+  return (
+    <div className="flex items-center gap-4 px-200 py-300">
+      <Avatar type="square">
+        {data?.club.profileImageUrl ? (
+          <AvatarImage width={64} height={64} alt="clubImage" src={data.club.profileImageUrl} />
+        ) : (
+          <AvatarFallback>
+            <Image src={BasicAvatarIcon} width={64} height={64} alt="기본 프로필" />
+          </AvatarFallback>
+        )}
+      </Avatar>
+      <div className="flex w-[172px] flex-col justify-center gap-[2px]">
+        <p className="text-text-strong typo-sub1">{data?.club.name}</p>
+        <p className="text-text-normal typo-body2">{data?.club.description}</p>
+      </div>
+    </div>
+  );
+}
