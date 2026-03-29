e5f4c89eb17c95ae4f563a84913f4816399c6e32
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-03-29 16:21:19 +0900

    feat: 기수 미 입력 모달 구현

---
+++ src/components/home/CardinalMissingModal.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogHeader,
+} from '@/components/ui';
+
+interface CardinalMissingModalProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+export function CardinalMissingModal({ open, onClose }: CardinalMissingModalProps) {
+  const router = useRouter();
+
+  // TODO: 기수 입력 페이지 경로 확정 후 변경
+  const handleComplete = () => {
+    router.push('/mypage');
+  };
+
+  return (
+    <AlertDialog open={open} onOpenChange={(isOpen) => !isOpen && onClose()} status="default">
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>기수를 입력해주셔야 합니다</AlertDialogTitle>
+          <AlertDialogDescription>
+            게시글 작성을 위해 기수 정보가 필요합니다.
+            <br />
+            기수 입력 페이지로 이동하시겠습니까?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogAction onClick={handleComplete}>지금 완성하기</AlertDialogAction>
+          <AlertDialogCancel onClick={onClose}>다음에 하기</AlertDialogCancel>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
