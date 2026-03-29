e753a1f5a32d99bf977fb0ac77c806f0fe5ea4cd
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-03-29 16:20:44 +0900

    feat: 알림 배너 컴포넌트 구현

--- src/components/home/AlertBanner.tsx
+++ src/components/home/AlertBanner.tsx
@@ -1,14 +1,23 @@
-import { AlertBannerCharIcon } from '@/assets/icons';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { CompleteIcon } from '@/assets/icons';
 import Image from 'next/image';
 
 export function AlertBanner() {
+  const router = useRouter();
+
   return (
-    <div className="bg-container-primary-alternative mt-300 flex gap-[10px] rounded-md p-300">
-      <Image src={AlertBannerCharIcon} width={40} height={40} alt="alert_banner_char" />
+    <button
+      type="button"
+      className="bg-container-primary-alternative mt-300 flex w-full cursor-pointer gap-[10px] rounded-md p-300 text-left"
+      onClick={() => router.push('/mypage')}
+    >
+      <Image src={CompleteIcon} width={40} height={40} alt="complete" />
       <div className="flex flex-col gap-100">
         <p className="typo-sub2 text-text-normal">나의 프로필을 완성하세요!</p>
         <p className="typo-body2 text-text-alternative">완성하고 게시글을 작성해요.</p>
       </div>
-    </div>
+    </button>
   );
 }
