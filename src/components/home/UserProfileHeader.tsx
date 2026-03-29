ce6e95abee73259e1915125a76d67b17cff11a0a
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-03-29 16:22:21 +0900

    feat: 유저 프로필 헤더 UI 구현 및 api 연동

---
+++ src/components/home/UserProfileHeader.tsx
@@ -0,0 +1,27 @@
+import { BasicAvatarIcon } from '@/assets/icons';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui';
+import { useHomeQuery } from '@/hooks/home';
+import Image from 'next/image';
+
+export function UserProfileHeader() {
+  const { data } = useHomeQuery();
+  const { userInfo, bio } = data?.myInfo ?? {};
+
+  return (
+    <div className="flex items-center gap-4 px-200 py-300">
+      <Avatar type="round">
+        {userInfo?.profileImageUrl ? (
+          <AvatarImage width={64} height={64} alt="프로필" src={userInfo.profileImageUrl} />
+        ) : (
+          <AvatarFallback>
+            <Image src={BasicAvatarIcon} width={64} height={64} alt="기본 프로필" />
+          </AvatarFallback>
+        )}
+      </Avatar>
+      <div className="flex w-[172px] flex-col justify-center gap-[2px]">
+        <p className="text-text-strong typo-sub1">{userInfo?.name}</p>
+        <p className="text-text-normal typo-body2">{bio ?? ''}</p>
+      </div>
+    </div>
+  );
+}
