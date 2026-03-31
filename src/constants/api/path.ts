7ae5fd86f1f1b2c91b028851ddbcce90cbcb8409
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-03-31 22:57:21 +0900

    fix: api path 재설정

--- src/constants/api/path.ts
+++ src/constants/api/path.ts
@@ -1,1 +1,4 @@
-export const API_BASE_PATH = '/api/v4';
+import { BASE_URL } from './env';
+
+export const API_BASE_PATH =
+  typeof window === 'undefined' ? `${BASE_URL}/api/v4` : '/api/proxy/api/v4';
