84a3e888fa7c239fcd805259537b5010cb28cfc8
nabbang6 <hawwah03@gachon.ac.kr>
2026-03-31 14:47:34 +0900

    refactor: 클라이언트 네비게이션 분리

---
+++ src/app/(private)/(main)/board/(with-nav)/BoardNavClient.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { BoardNav } from '@/components/board';
+import { useActiveBoardId, useSetActiveBoardId } from '@/stores/useBoardNavStore';
+import type { BoardNavItem } from '@/components/board';
+
+interface BoardNavClientProps {
+  items: BoardNavItem[];
+}
+
+function BoardNavClient({ items }: BoardNavClientProps) {
+  const activeBoardId = useActiveBoardId();
+  const setActiveBoardId = useSetActiveBoardId();
+
+  return (
+    <BoardNav
+      items={items}
+      activeId={activeBoardId}
+      onItemSelect={setActiveBoardId}
+    />
+  );
+}
+
+export { BoardNavClient };
