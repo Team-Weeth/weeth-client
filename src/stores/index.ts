0f944d826bbc4c06959e25016aab210455c9cbba
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-03-31 22:47:27 +0900

    fix: 유저 정보에 프로필 사진까지 추가

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -3,4 +3,11 @@
 export { useThemeStore } from './theme-store';
 export { useAuthStore, useAuthName, useAuthProfileImage, useAuthActions } from './useAuthStore';
 export { useClubStore, useClubId, useClubActions } from './useClubStore';
-export { useUserStore, useUserId, useUserName, useUserRole, useUserActions } from './useUserStore';
+export {
+  useUserStore,
+  useUserId,
+  useUserName,
+  useUserProfileImageUrl,
+  useUserRole,
+  useUserActions,
+} from './useUserStore';
--- src/stores/useUserStore.ts
+++ src/stores/useUserStore.ts
@@ -4,6 +4,7 @@ import { combine, devtools } from 'zustand/middleware';
 const initialState = {
   id: null as number | null,
   name: null as string | null,
+  profileImageUrl: null as string | null,
   role: null as 'LEAD' | 'USER' | null,
 };
 
@@ -12,7 +13,12 @@ export type UserState = typeof initialState;
 export const useUserStore = create(
   devtools(
     combine(initialState, (set) => ({
-      setUser: (user: { id: number; name: string; role: 'LEAD' | 'USER' }) =>
+      setUser: (user: {
+        id: number;
+        name: string;
+        profileImageUrl: string | null;
+        role: 'LEAD' | 'USER';
+      }) =>
         set(user, false, 'setUser'),
       reset: () => set(initialState, false, 'reset'),
     })),
@@ -22,6 +28,7 @@ export const useUserStore = create(
 
 export const useUserId = () => useUserStore((store) => store.id);
 export const useUserName = () => useUserStore((store) => store.name);
+export const useUserProfileImageUrl = () => useUserStore((store) => store.profileImageUrl);
 export const useUserRole = () => useUserStore((store) => store.role);
 export const useUserActions = () =>
   useUserStore((store) => ({
