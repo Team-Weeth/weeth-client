3ea234ab601cea76c12e51b527f1cd079840b242
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-03-31 22:45:45 +0900

    fix: 최근 게시글 pageSize 넘겨주도록 수정

--- src/hooks/home/useRecentPostsQuery.ts
+++ src/hooks/home/useRecentPostsQuery.ts
@@ -2,12 +2,17 @@ import { useQuery } from '@tanstack/react-query';
 import { homeApi } from '@/lib/apis/home';
 import { useClubId } from '@/stores/useClubStore';
 
+const HOME_RECENT_POSTS_PAGE_SIZE = 10;
+
 export function useRecentPostsQuery() {
   const clubId = useClubId();
 
   return useQuery({
     queryKey: ['home', 'recent-posts', clubId],
-    queryFn: () => homeApi.getRecentPosts(clubId!).then((res) => res.data.data),
+    queryFn: () =>
+      homeApi
+        .getRecentPosts(clubId!, { pageSize: HOME_RECENT_POSTS_PAGE_SIZE })
+        .then((res) => res.data.data),
     enabled: !!clubId,
   });
 }
