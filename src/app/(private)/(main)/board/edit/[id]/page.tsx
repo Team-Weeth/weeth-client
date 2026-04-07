cb09526514695fff7ec43e749bea97d4950aa2c4
nabbang6 <hawwah03@gachon.ac.kr>
2026-04-07 18:47:44 +0900

    fix: 게시글 수정 페이지에 유효하지 않은 ID 에러 처리 추가

--- src/app/(private)/(main)/board/edit/[id]/page.tsx
+++ src/app/(private)/(main)/board/edit/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from 'next/navigation';
+
 import { boardServerApi } from '@/lib/apis/board.server';
 import { EditClientEditor } from './EditClientEditor';
 
@@ -7,8 +9,18 @@ interface PostEditPageProps {
 
 export default async function PostEditPage({ params }: PostEditPageProps) {
   const { id } = await params;
+  const postId = Number(id);
+
+  if (Number.isNaN(postId)) {
+    notFound();
+  }
+
   // TODO: 추후 하드코딩된 clubId 제거 예정
-  const response = await boardServerApi.getPostById('YUNJcjFKMO', Number(id));
+  const response = await boardServerApi.getPostById('YUNJcjFKMO', postId).catch(() => null);
+
+  if (!response?.data) {
+    notFound();
+  }
 
   return (
     <main className="w-full">
