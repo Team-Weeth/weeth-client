f72541b597b23267dfbdd30120a1aea35a3bab59
nabbang6 <hawwah03@gachon.ac.kr>
2026-04-07 18:17:39 +0900

    fix: 코드 스플리팅 오버헤드 수정

--- src/components/board/PostActionMenu.tsx
+++ src/components/board/PostActionMenu.tsx
@@ -1,12 +1,8 @@
 'use client';
 
 import { useState } from 'react';
-import dynamic from 'next/dynamic';
 import { ActionMenu, type ActionMenuProps } from './ActionMenu';
-
-const PostDeleteDialog = dynamic(() =>
-  import('./PostDeleteDialog').then((mod) => mod.PostDeleteDialog),
-);
+import { PostDeleteDialog } from './PostDeleteDialog';
 
 interface PostActionMenuProps extends Omit<ActionMenuProps, 'onDeleteSelect'> {
   postId: number;
@@ -15,9 +11,6 @@ interface PostActionMenuProps extends Omit<ActionMenuProps, 'onDeleteSelect'> {
 
 /**
  * 게시글 전용 수정/삭제 메뉴
- *
- * 삭제 선택 시 확인 다이얼로그를 띄우고 내부에서 deletePost API를 호출
- * 댓글/답글 등 범용 용도에는 `ActionMenu`를 사용
  */
 function PostActionMenu({ postId, onDeleted, ...rest }: PostActionMenuProps) {
   const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
