b464a50f03c916862ed8599eebc27d7b8c91f18c
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-04-08 11:01:46 +0900

    fix: 랜딩 이미지 svg -> png로 변경

--- src/assets/image/landing/card/index.ts
+++ src/assets/image/landing/card/index.ts
@@ -1,10 +1,8 @@
-export { default as LandingCard1 } from './landing_card_1.svg';
-export { default as LandingCard2 } from './landing_card_2.svg';
-export { default as LandingCard3 } from './landing_card_3.svg';
-export { default as LandingCard4 } from './landing_card_4.svg';
-export { default as LandingCard5 } from './landing_card_5.svg';
-export { default as LandingCard6 } from './landing_card_6.svg';
-export { default as LandingCard7 } from './landing_card_7.svg';
-export { default as LandingCard8 } from './landing_card_8.svg';
-
-export { default as LandingHeroCard } from './landing_hero_card.svg';
+export { default as LandingCard1 } from './landing_card_1.png';
+export { default as LandingCard2 } from './landing_card_2.png';
+export { default as LandingCard3 } from './landing_card_3.png';
+export { default as LandingCard4 } from './landing_card_4.png';
+export { default as LandingCard5 } from './landing_card_5.png';
+export { default as LandingCard6 } from './landing_card_6.png';
+export { default as LandingCard7 } from './landing_card_7.png';
+export { default as LandingCard8 } from './landing_card_8.png';
