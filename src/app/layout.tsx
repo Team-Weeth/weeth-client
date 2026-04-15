872e175f0858314020ca09bbe8d83d90e195bd21
JIN921 <wlslwlsl808@gmail.com>
2026-04-15 20:21:45 +0900

    fix: og 메타데이터 metadataBase 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://www.landing.weeth.kr'),
   title: '우리 동아리만의 사이트를 원한다면, 위드',
   description:
     '동아리의 출석부터 활동 공유까지! 위드에서 동아리만의 사이트를 개설하여 관리해보세요.',
