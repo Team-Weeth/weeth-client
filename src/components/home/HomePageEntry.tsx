5a55ea768da5517e029f982129f104576b21f508
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-04-27 17:33:06 +0900

    fix: 홈 페이지 스켈레톤 UI에 맞게 적용

--- src/components/home/HomePageEntry.tsx
+++ src/components/home/HomePageEntry.tsx
@@ -22,18 +22,17 @@ function HomePageSkeleton() {
   return (
     <>
       <BannerSkeleton />
-      <div className="h-[18px]" />
-      <div className="tablet:flex-row desktop:px-16 flex w-full flex-col gap-8 px-[18px]">
+      <div className="tablet:flex-row desktop:px-16 flex w-full flex-col gap-8 px-[18px] py-450">
         <div className="tablet:w-[304px] tablet:shrink-0 flex flex-col gap-300">
           <LeftContainerSkeleton />
           <div className="desktop:hidden tablet:flex hidden flex-col gap-300">
             <RightContainerSkeleton />
           </div>
         </div>
-        <div className="flex w-full flex-col gap-300">
+        <div className="flex min-w-0 flex-1 flex-col gap-300">
           <MainContainerSkeleton />
         </div>
-        <div className="desktop:flex hidden flex-col gap-300">
+        <div className="desktop:flex desktop:w-[339px] desktop:shrink-0 tablet:hidden flex flex-col gap-300">
           <RightContainerSkeleton />
         </div>
       </div>
