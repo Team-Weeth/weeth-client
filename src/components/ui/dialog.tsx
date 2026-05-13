20eb0fe06eb15e7c0cd6b2472ff0f6b2241ea06a
JIN921 <wlslwlsl808@gmail.com>
2026-05-13 15:57:54 +0900

    feat: 스케줄 모달 반응형 적용

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -97,17 +97,18 @@ function DialogContent({
         data-slot="dialog-content"
         className={cn(
           'bg-container-neutral data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-[80] grid w-78.75 max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] rounded-lg border p-400 shadow-lg duration-200 outline-none sm:max-w-lg',
-          isAdminScope &&
-            'max-tablet:inset-0 max-tablet:top-0 max-tablet:left-0 max-tablet:h-screen max-tablet:w-screen max-tablet:max-w-none max-tablet:translate-x-0 max-tablet:translate-y-0 max-tablet:rounded-none max-tablet:border-0 max-tablet:p-0',
+          // isAdminScope &&
+          //   'max-tablet:inset-0 max-tablet:top-0 max-tablet:left-0 max-tablet:h-screen max-tablet:w-screen max-tablet:max-w-none max-tablet:translate-x-0 max-tablet:translate-y-0 max-tablet:rounded-none max-tablet:border-0 max-tablet:p-0',
           className,
         )}
         {...props}
       >
-        {isAdminScope ? (
+        {children}
+        {/* {isAdminScope ? (
           <AdminMobileBlockedContent>{children}</AdminMobileBlockedContent>
         ) : (
           children
-        )}
+        )} */}
         {showCloseButton && (
           <DialogPrimitive.Close
             data-slot="dialog-close"
