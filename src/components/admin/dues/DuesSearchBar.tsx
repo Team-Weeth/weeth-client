9eab223ead4f6c24b7b560db782d28825ca94871
JIN921 <wlslwlsl808@gmail.com>
2026-06-28 23:03:50 +0900

    chore: Prettier 포맷 전체 적용

--- src/components/admin/dues/DuesSearchBar.tsx
+++ src/components/admin/dues/DuesSearchBar.tsx
@@ -17,7 +17,7 @@ function DuesSearchBar({ searchQuery, setSearchQuery }: DuesSearchBarProps) {
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
         placeholder="이름으로 검색하기"
-         aria-label="이름으로 검색하기"
+        aria-label="이름으로 검색하기"
         className="typo-body2 placeholder:text-text-alternative text-text-strong h-full w-full bg-transparent pr-400 pl-[52px] outline-none"
       />
     </div>
