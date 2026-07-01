224a4dcbd02e6676466eee5e0c050a59a5d28cef
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-07-01 17:26:39 +0900

    feat: 마이페이지 모바일 반응형 구현

--- src/components/mypage/MyPageContent.tsx
+++ src/components/mypage/MyPageContent.tsx
@@ -6,6 +6,9 @@ import { useMyPageQueries } from '@/hooks/queries/mypage/useMyPageQueries';
 import { ProfileSection } from './ProfileSection';
 import { ProfileSectionSkeleton } from './skeleton';
 import { ActiveClubList } from './ActiveClubList';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage } from '../ui';
+import { MyPageActivityContent } from './MyPageActivityContent';
+import { MyPageSettingsContent } from './MyPageSettingsContent';
 
 type MyPageContentProps = React.HTMLAttributes<HTMLDivElement>;
 
@@ -15,23 +18,66 @@ function MyPageContent({ className, ...props }: MyPageContentProps) {
   const displayName = me?.name ?? '';
 
   return (
-    <div className={cn('flex min-w-0 flex-1 flex-col gap-4', className)} {...props}>
-      <p className="typo-h3 text-text-normal">프로필</p>
-      {me ? (
-        <ProfileSection
-          name={displayName}
-          bio={me.bio ?? undefined}
-          profileImageUrl={me.profileImageUrl ?? undefined}
-          tel={me.tel ?? undefined}
-          email={me.email ?? undefined}
-          school={me.school ?? undefined}
-          department={me.department ?? undefined}
-        />
-      ) : (
-        <ProfileSectionSkeleton />
-      )}
-      <ActiveClubList clubs={clubs} clubId={clubId} />
-    </div>
+    <>
+      <div className={cn('tablet:flex hidden min-w-0 flex-1 flex-col gap-4', className)} {...props}>
+        <p className="typo-h3 text-text-normal">프로필</p>
+        {me ? (
+          <ProfileSection
+            name={displayName}
+            bio={me.bio ?? undefined}
+            profileImageUrl={me.profileImageUrl ?? undefined}
+            tel={me.tel ?? undefined}
+            email={me.email ?? undefined}
+            school={me.school ?? undefined}
+            department={me.department ?? undefined}
+          />
+        ) : (
+          <ProfileSectionSkeleton />
+        )}
+        <ActiveClubList clubs={clubs} clubId={clubId} />
+      </div>
+      <div
+        className={cn('tablet:hidden flex min-w-0 flex-1 flex-col gap-200', className)}
+        {...props}
+      >
+        <div className="flex flex-col gap-200">
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbPage className="typo-caption1 text-text-alternative">
+                  프로필
+                </BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+          <p className="typo-h2 text-text-normal">프로필</p>
+        </div>
+        <div className="flex flex-col gap-500">
+          <div className="flex flex-col gap-4">
+            {me ? (
+              <ProfileSection
+                name={displayName}
+                bio={me.bio ?? undefined}
+                profileImageUrl={me.profileImageUrl ?? undefined}
+                tel={me.tel ?? undefined}
+                email={me.email ?? undefined}
+                school={me.school ?? undefined}
+                department={me.department ?? undefined}
+              />
+            ) : (
+              <ProfileSectionSkeleton />
+            )}
+            <ActiveClubList clubs={clubs} clubId={clubId} />
+          </div>
+          <div className="flex flex-col gap-4">
+            <MyPageActivityContent />
+          </div>
+          <div className="flex flex-col gap-4">
+            <MyPageSettingsContent />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
