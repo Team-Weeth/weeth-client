a09eb0190678a1383fb0b8406820aaccaa94de60
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-07-25 23:35:59 +0900

    fix: 스크롤 초기화가 여전히 밀리는 문제 - requestAnimationFrame 중첩 처리로 보강

--- src/hooks/useScrollToTopOnNavigate.ts
+++ src/hooks/useScrollToTopOnNavigate.ts
@@ -7,8 +7,14 @@ function useScrollToTopOnNavigate() {
   const pathname = usePathname();
 
   useEffect(() => {
-    const id = requestAnimationFrame(() => window.scrollTo(0, 0));
-    return () => cancelAnimationFrame(id);
+    let innerId = 0;
+    const outerId = requestAnimationFrame(() => {
+      innerId = requestAnimationFrame(() => window.scrollTo(0, 0));
+    });
+    return () => {
+      cancelAnimationFrame(outerId);
+      cancelAnimationFrame(innerId);
+    };
   }, [pathname]);
 }
 
