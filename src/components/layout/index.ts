3fbd5de3bd225151a2204c2488c799e1b39633a7
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-07-25 23:07:15 +0900

    feat: 페이지 이동 시 스크롤 위치가 유지되는 문제 수정

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './header/Header';
 export { default as Footer } from './Footer';
 export { default as PublicHeader } from './header/PublicHeader';
+export { ScrollRestoration } from './ScrollRestoration';
