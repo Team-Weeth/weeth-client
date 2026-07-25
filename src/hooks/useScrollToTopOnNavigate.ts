b2ea8840027c1c9aee02c75e1b12838d7d87fe0b
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-07-25 23:08:10 +0900

    feat: 페이지 이동 시 스크롤 위치가 유지되는 문제 수정

---
+++ src/hooks/useScrollToTopOnNavigate.ts
@@ -0,0 +1,14 @@
+'use client';
+
+import { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
+
+function useScrollToTopOnNavigate() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+}
+
+export { useScrollToTopOnNavigate };
