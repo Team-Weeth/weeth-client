ec2760bc418350897bd0183b65330b1c14ee3401
nabbang6 <hawwah03@gachon.ac.kr>
2026-09-02 20:27:47 +0900

    refactor: 캘린더 유틸 함수를 utils/shared/calendar.ts로 분리

--- src/components/calendar/CalendarMini.tsx
+++ src/components/calendar/CalendarMini.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/cn';
 import { Icon } from '@/components/ui/Icon';
 import { ArrowLeftIcon, ArrowRightIcon } from '@/assets/icons';
 import { DAY_META, DAY_HEADER_COLOR } from '@/constants/shared/date';
-import { buildCalendarCells, getCalendarCellColors } from '@/utils/shared/date';
+import { buildCalendarCells, getCalendarCellColors } from '@/utils/shared/calendar';
 import {
   useCalendarYear,
   useCalendarMonth,
