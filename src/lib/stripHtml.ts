0522f7b1619b97aa6f15a3eb9ada6c8726ef87fa
nabbang6 <hawwah03@gachon.ac.kr>
2026-09-23 18:07:06 +0900

    fix: stripHtml 테이블 셀 구분 처리 및 HTML 엔티티 디코딩 추가

--- src/lib/stripHtml.ts
+++ src/lib/stripHtml.ts
@@ -2,8 +2,16 @@
 export function stripHtml(html: string): string {
   return html
     .replace(/<br\s*\/?>/gi, '\n')
-    .replace(/<\/(p|h[1-6]|li|div|blockquote)>/gi, '\n')
+    .replace(/<\/(p|h[1-6]|li|div|blockquote|td|th|tr)>/gi, '\n')
     .replace(/<[^>]*>/g, '')
+    .replace(/&nbsp;/gi, ' ')
+    .replace(/&lt;/gi, '<')
+    .replace(/&gt;/gi, '>')
+    .replace(/&quot;/gi, '"')
+    .replace(/&apos;|&#39;/gi, "'")
+    .replace(/&#(\d+);/g, (_, dec) => String.fromCharCode(Number(dec)))
+    .replace(/&#x([0-9a-f]+);/gi, (_, hex) => String.fromCharCode(parseInt(hex, 16)))
+    .replace(/&amp;/gi, '&')
     .replace(/\n{3,}/g, '\n\n')
     .trim();
 }
